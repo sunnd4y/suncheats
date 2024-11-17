e20bb82a92eddef0eb4a78ca3d4ee5f845a0279f
sunnd4y <120350465+sunnd4y@users.noreply.github.com>
2024-11-17 15:44:29 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -3,9 +3,9 @@ local test = {}
 setmetatable(test, {
     __call = function(_, args)
         if args[1] == "NEXT GEN" then
-            print("Welcome to the Next Generation!")
+            print("Welcome to suncheats Next Generation!")
         else
-            print("Welcome to the Old Generation.")
+            print("Welcome to suncheats Old Generation. (Probably Outdated)")
         end
     end
 })
