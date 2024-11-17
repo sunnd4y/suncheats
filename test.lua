0e2d4f631b56da4e2c8abbf5954b521e63fc6522
sunnd4y <120350465+sunnd4y@users.noreply.github.com>
2024-11-17 15:40:56 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,14 @@
-print("test")
+local test = {}
+
+setmetatable(test, {
+    __call = function(_, args)
+        if args[1] == "NEXT GEN" then
+            print("Welcome to the Next Generation!")
+        else
+            print("Welcome to the Old Generation.")
+        end
+    end
+})
+
+getgenv().test = test
+return test
